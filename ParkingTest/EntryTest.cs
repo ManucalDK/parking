c51af791ff89bd375cb655decc4d7e46d9c6f8be
andres.ruales <andres.ruales@ceiba.com.co>
2021-01-14 19:12:48 -0500

    configuracion gitignore y creacion proyecto test

---
+++ ParkingTest/EntryTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParkingTest
+{
+    [TestClass]
+    public class EntryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
