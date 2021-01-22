a1afec3f6051981ba7d438bdffd543dc96507163
andres.ruales <andres.ruales@ceiba.com.co>
2021-01-22 11:27:50 -0500

    implementacion de test a departure entity

--- ParkingTest/Unit/Services/DepartureServiceTests.cs
+++ ParkingTest/Unit/Services/DepartureServiceTests.cs
@@ -9,6 +9,7 @@ using Moq;
 using ParkingTest.Builders;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ParkingTest.Unit.Services
 {
@@ -113,5 +114,164 @@ namespace ParkingTest.Unit.Services
                 throw;
             }
         }
+
+        [TestMethod()]
+        public void GetDepartures_WithData_ShouldReturnListWithValues()
+        {
+            // Arrange
+            _departureRepository.Setup(r => r.List()).Returns(new List<DepartureEntity> {
+                new DepartureEntity
+                {
+                    DepartureTime = DateTime.Now,
+                    Id = Guid.NewGuid().ToString()
+                }
+            });
+            var entryService = new DepartureService(_departureRepository.Object, _entryRepository.Object, _rateService.Object, _cellService.Object);
+
+
+            // Act
+            var result = entryService.GetDepartures();
+
+            // Assert
+            Assert.IsTrue(result.Count() > 0);
+        }
+
+        [TestMethod()]
+        public void GetDepartures_WithoutData_ShouldReturnEmptyList()
+        {
+            // Arrange
+            _departureRepository.Setup(r => r.List()).Returns(new List<DepartureEntity>());
+            var entryService = new DepartureService(_departureRepository.Object, _entryRepository.Object, _rateService.Object, _cellService.Object);
+
+
+            // Act
+            var result = entryService.GetDepartures();
+
+            // Assert
+            Assert.IsTrue(result.Count() <= 0);
+        }
+
+        [TestMethod()]
+        public void GetEntryById_ShouldReturn_DtoDepartureEntity()
+        {
+            // Arrange
+            DepartureEntity departureEntity = new DepartureEntityBuilder()
+                                                .WithId(Guid.NewGuid().ToString())
+                                                .Build();
+
+            _departureRepository.Setup(r => r.GetById(departureEntity.Id)).Returns(departureEntity);
+
+            var entryService = new DepartureService(_departureRepository.Object, _entryRepository.Object, _rateService.Object, _cellService.Object);
+
+            // Act
+            DTODeparture result =  entryService.GetEntryById(id: departureEntity.Id);
+
+            // Assert
+            Assert.IsNotNull(result);
+
+        }
+
+        [TestMethod()]
+        public void GetEntryByBadId_ShouldReturn_DtoEmpty()
+        {
+            // Arrange
+            var newId = Guid.NewGuid().ToString();
+            _departureRepository.Setup(r => r.GetById(newId));
+
+            var entryService = new DepartureService(_departureRepository.Object, _entryRepository.Object, _rateService.Object, _cellService.Object);
+
+            // Act
+            DTODeparture result = entryService.GetEntryById(id: newId);
+
+            // Assert
+            Assert.IsNull(result.Id);
+        }
+
+        [TestMethod()]
+        public void GetDepartureByEntryId_ShouldReturn_DtoDepartureEntity()
+        {
+            // Arrange
+            DepartureEntity departureEntity = new DepartureEntityBuilder()
+                                                .WithId(Guid.NewGuid().ToString())
+                                                .Build();
+
+            var id = departureEntity.Id;
+
+            _departureRepository.Setup(r => r.List(dr => dr.IdEntry == id)).Returns(new List<DepartureEntity> { departureEntity });
+
+            var entryService = new DepartureService(_departureRepository.Object, _entryRepository.Object, _rateService.Object, _cellService.Object);
+
+            // Act
+            DepartureEntity result = entryService.GetDepartureByEntryId(id);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void GetEntryByBadId_ShouldReturn_Null()
+        {
+            // Arrange
+            DepartureEntity departureEntity = new DepartureEntityBuilder()
+                                                .WithId(Guid.NewGuid().ToString())
+                                                .Build();
+
+            var id = Guid.NewGuid().ToString();
+
+            _departureRepository.Setup(r => r.List(dr => dr.IdEntry == id)).Returns(new List<DepartureEntity>());
+
+            var entryService = new DepartureService(_departureRepository.Object, _entryRepository.Object, _rateService.Object, _cellService.Object);
+
+            // Act
+            DepartureEntity result = entryService.GetDepartureByEntryId(id);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [TestMethod()]
+        public void GetDepartureByVehicleId_ShouldReturn_DtoDepartureEntity()
+        {
+            // Arrange
+            DepartureEntity departureEntity = new DepartureEntityBuilder()
+                                                .WithId(Guid.NewGuid().ToString())
+                                                .WithIdVehicle("ASD123")
+                                                .Build();
+
+            var vehicleId = departureEntity.IdVehicle;
+
+            _departureRepository.Setup(r => r.List(dr => dr.IdVehicle == vehicleId)).Returns(new List<DepartureEntity> { departureEntity });
+
+            var entryService = new DepartureService(_departureRepository.Object, _entryRepository.Object, _rateService.Object, _cellService.Object);
+
+            // Act
+            DepartureEntity result = entryService.GetDepartureByVehicleId(vehicleId);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void GetDepartureByVehicleId_ShouldReturn_Null()
+        {
+            // Arrange
+            DepartureEntity departureEntity = new DepartureEntityBuilder()
+                                                .WithId(Guid.NewGuid().ToString())
+                                                .WithIdVehicle("ASD123")
+                                                .Build();
+
+            var vehicleId = "AAA111";
+
+            _departureRepository.Setup(r => r.List(dr => dr.IdVehicle == vehicleId)).Returns(new List<DepartureEntity>());
+
+            var entryService = new DepartureService(_departureRepository.Object, _entryRepository.Object, _rateService.Object, _cellService.Object);
+
+            // Act
+            DepartureEntity result = entryService.GetDepartureByVehicleId(vehicleId);
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
     }
 }
